a906d0ccbceaa75107954b6dd8d8682b703d42df
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-08-02 16:33:48 -0400

    requisito 5

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-document.getElementsByClassName('color')[0].style.backgroundColor = "#000000"
+let color1 = document.getElementsByClassName('color')[0];
+color1.style.backgroundColor = "#000000";
+color1.classList.add('selected');
 
-document.getElementsByClassName('color')[1].style.backgroundColor = "#e9311a"
+let color2 = document.getElementsByClassName('color')[1];
+color2.style.backgroundColor = "#e9311a";
 
-document.getElementsByClassName('color')[2].style.backgroundColor = "#ed6335"
+let color3 = document.getElementsByClassName('color')[2];
+color3.style.backgroundColor = "#ed6335";
 
-document.getElementsByClassName('color')[3].style.backgroundColor = "#ecae7d"
+let color4 = document.getElementsByClassName('color')[3];
+color4.style.backgroundColor = "#ecae7d";
+
 
