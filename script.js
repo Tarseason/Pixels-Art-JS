dbdacc4338327cbf1d7f956fbc2b1b16465628e4
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-08-02 15:15:20 -0400

    requerimento 4

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ document.getElementsByClassName('color')[1].style.backgroundColor = "#e9311a"
 document.getElementsByClassName('color')[2].style.backgroundColor = "#ed6335"
 
 document.getElementsByClassName('color')[3].style.backgroundColor = "#ecae7d"
+
